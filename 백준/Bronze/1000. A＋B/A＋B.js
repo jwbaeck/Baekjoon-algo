f5b6b0cb59d9abfe669172c5d187d88d0c90d690
jwbaeck <137036757+jwbaeck@users.noreply.github.com>
2023-10-25 21:41:01 -0700

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split(“\n”);
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+// let input = fs.readFileSync(filePath).toString().trim().split(“”);
+input = input.map((value) => Number(value));
+
+console.log(input.reduce((p, n) => p + n));
