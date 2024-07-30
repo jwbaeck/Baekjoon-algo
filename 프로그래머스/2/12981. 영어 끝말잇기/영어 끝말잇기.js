c5a5595890c17d5aa4a7c5d6f7bbb4c05955b918
jwbaeck <jwbaeck97@gmail.com>
2024-07-31 06:06:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,17 @@
+function solution(n, words) {
+  let usedWords = new Set();  
+  let lastChar = words[0][0]; 
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (word.length === 1 || word[0] !== lastChar || usedWords.has(word)) {
+      return [(i % n) + 1, Math.floor(i / n) + 1];
+    }
+
+    usedWords.add(word);
+    lastChar = word[word.length - 1];
+  }
+
+  return [0, 0];
+}
