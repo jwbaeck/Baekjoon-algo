d4b824bda2042226671dbaf80d28c4e4b55982f9
jwbaeck <jwbaeck97@gmail.com>
2024-08-13 13:08:33 +0900

    [level 2] Title: 구명보트, Time: 13.16 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,20 @@
+function solution(people, limit) {
+  const sortedPeople = [...people].sort((a, b) => a - b);
+
+  let left = 0;
+  let right = sortedPeople.length - 1;
+  let boats = 0;
+
+  while (left <= right) {
+    if (sortedPeople[left] + sortedPeople[right] <= limit) {
+      left++;
+      right--;
+    } else {
+      right--;
+    }
+
+    boats++;
+  }
+
+  return boats;
+}
