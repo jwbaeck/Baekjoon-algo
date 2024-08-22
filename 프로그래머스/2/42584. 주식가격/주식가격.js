23a40ea1ddc9d24f7f9780311e3ed79e0b030d0f
jwbaeck <jwbaeck97@gmail.com>
2024-08-22 21:26:30 +0900

    [level 2] Title: 주식가격, Time: 34.40 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,14 @@
+function solution(prices) {
+  const result = new Array(prices.length).fill(0);
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      result[i]++;
+      if (prices[i] > prices[j]) {
+          break;
+      }
+    }
+  }
+
+  return result;
+}
